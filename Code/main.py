15236754c8017224c4548b6213fc14369c1b608d
5Sevenx <dogecool2004@gmail.com>
2025-09-11 05:41:42 -0800

    g channel added

--- Code/main.py
+++ Code/main.py
@@ -42,24 +42,46 @@ cntG = 0
 @bot.event
 async def on_voice_state_update(member, before, after):
     global cntC
+    global cntG
     guild = member.guild
 
+    # --- Chill ---
     if after.channel and after.channel.id == CId:
         cntC += 1
         new_channel = await guild.create_voice_channel(
             name=f'c{cntC}' ,category=after.channel.category
         )
-
         temp_channelsC.append(new_channel.id)
-
         await member.move_to(new_channel)
+    # --- Chill ---
 
+    # --- Game  ---
+    elif after.channel and after.channel.id == GId:
+        cntG += 1
+        new_channel = await guild.create_voice_channel(
+            name=f'g{cntG}' ,category=after.channel.category
+        )
+        temp_channelsG.append(new_channel.id)
+        await member.move_to(new_channel)
+    # --- Game  ---
+
+    # --- Game  ---
+    for channel_id in temp_channelsG:
+        channel = guild.get_channel(channel_id)
+        if len(channel.members) == 0:
+            await channel.delete()
+            temp_channelsG.remove(channel_id)
+            cntG -= 1
+    # --- Game  ---
+
+
+    # --- Chill ---
     for channel_id in temp_channelsC:
         channel = guild.get_channel(channel_id)
         if len(channel.members) == 0:
             await channel.delete()
             temp_channelsC.remove(channel_id)
             cntC -= 1
-
+    # --- Chill ---
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
