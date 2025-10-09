6ccd98af48f6c94604e84dc1f7f91b1d4114c85c
5Sevenx <dogecool2004@gmail.com>
2025-10-09 05:09:44 -0800

    missing arguments logic

--- Code/MSG/messages.py
+++ Code/MSG/messages.py
@@ -13,17 +13,40 @@ class MissingParameters(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
+        command = ctx.message.content[1:]
+        if command == "spy":
+            allowed_channel= int(data["SPY_CHANNEL_ID"])
+            if ctx.channel.id != allowed_channel:
+                embed = Embed(
+                title=data["FCNF"],
+                description = data["JTSAC"],
+                color = 0x7F00FF
+                )
+                embed.add_field(name=data["L"], value=data["CHL"])
+                await ctx.message.delete()
+                await ctx.channel.send(embed=embed, delete_after=DeleteMSGTiming)
+                return
+
+
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send('Missing required argument')
-        #TODO:add more logic
+            embed = Embed(
+                title="Missing Argument",
+                description=data["MISSA"].format(error=error, ctx=ctx),
+                colour=0x7F00FF
+            )
+            await ctx.message.delete()
+            await ctx.channel.send(embed=embed, delete_after=DeleteMSGTiming)
+
 
         elif isinstance(error, commands.CommandNotFound):
-            command = ctx.message.content[1:]
+
             embed = Embed(
-                title=data(["CNF"]).format(command=command),
+                title=data["CNF"].format(command=command),
                 description=data["JTSAC"],
                 color=0x7F00FF
             )
             embed.add_field(name=data["L"], value=data["CHL"])
             await ctx.message.delete()
             await ctx.channel.send(embed=embed, delete_after=DeleteMSGTiming)
+
+#TODO:Help command to help with commands n arguments
